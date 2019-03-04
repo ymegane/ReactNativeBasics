756a818ebafdad7b790e5f81caa77d3420ad9b79
ymegane <crybaby.sumire+github@gmail.com>
2019-03-04 22:56:32 +0900

    Add Flexbox component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@
 
 import React, { Component } from 'react';
 import {Platform, StyleSheet, Text, SafeAreaView} from 'react-native';
-import Styles from './components/Styles';
+import FlexDimensionsBasics from './components/FlexDimentionsBasics';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -23,7 +23,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <Styles />
+        <FlexDimensionsBasics />
       </SafeAreaView>
     );
   }
